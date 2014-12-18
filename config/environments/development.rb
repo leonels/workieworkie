0e011a42ade652fff06c3d455c51f9b5ac9a13b1
Leonel Santos <leonel.santos@rackspace.com>
2014-12-17 20:17:35 -0600

    config(rails): set log level for development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,9 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  # Set log level 
+  config.log_level = :info
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
